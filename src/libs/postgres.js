75bd082daf83af93ec0429bef9732f62ffe3b9b0
Dimas Mendoza <damendoza98@gmail.com>
2024-02-23 10:02:04 -0500

    Agrega dependencia dotenv y actualiza configuración de PostgreSQL

--- src/libs/postgres.js
+++ src/libs/postgres.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'dpg-cnbtps779t8c73ep08j0-a',
-    port: 5432,
-    user: 'storagevehicle',
-    password: 'Ue7yPGZjpDr5d6oYtlY4rRH239OscEPR',
-    database: 'storeapivehiclesv2'
-})
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
+});
 
-module.exports = { pool }
+module.exports = { pool };
 
