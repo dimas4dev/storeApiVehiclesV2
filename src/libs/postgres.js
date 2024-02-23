4be735fe8872781e62a39b4044ab8f78203f6f6b
Dimas Mendoza <damendoza98@gmail.com>
2024-02-22 19:39:43 -0500

    Actualizado el host de la base de datos de PostgreSQL en postgres.js

--- src/libs/postgres.js
+++ src/libs/postgres.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: 'Ue7yPGZjpDr5d6oYtlY4rRH239OscEPR@dpg-cnbtps779t8c73ep08j0-a/storeapivehiclesv2',
+    host: 'Ue7yPGZjpDr5d6oYtlY4rRH239OscEPR@dpg-cnbtps779t8c73ep08j0-a.oregon-postgres.render.com',
     port: 5432,
     user: 'storagevehicle',
     password: 'Ue7yPGZjpDr5d6oYtlY4rRH239OscEPR',
